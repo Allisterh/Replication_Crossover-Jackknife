219282bc500e07be157b41fd56348847dd2ea4f5
Shuowen Chen <swchen@bu.edu>
2020-06-06 15:37:08 -0400

    Changing code

---
+++ Simulation Code/auxiliary.R
@@ -0,0 +1,178 @@
+####################################################################
+# MONTE CARLO CALIBRATED TO LFP:  DYNAMIC PROBIT MODEL
+####################################################################
+#  14.382 L10 MIT.  V. Chernozhukov and I. Fernandez-Val
+#  and Shuowen Chen
+# Data source: Ivan Fernandez-Val, "Fixed effects estimation of structural parameters and marginal effects in 
+# panel probit models," Journal of Econometrics, Elsevier, vol. 150(1), pages 71-85, May.
+
+# The variables in the data set include:
+#
+# id = "woman identifier"
+# year = "year"
+# lfp = "= 1 if woman participates in labor force"
+# laglfp = "lag of lfp"
+# kids0_2 = "number of children of ages <= 2 years"
+# kids3_5 = "number of children of ages > 2 years and < 6 years"
+# kids6_17 = "number of children of ages > 5 years and < 18 years"
+# loghusbandincome = "log of husband income ($1000 of 1995)"
+# age = "age"
+# age2 = "age squared"
+###################################################################
+
+### set working directory
+filepath <- "/Users/shuowenchen/desktop/Research/Panel-Cross-over-jackknife/Crossover-Jackknife/Simulation Code"
+setwd(filepath)
+
+# Load functions to estimate probit fast and perform bias corrections;
+
+library(foreign)
+library(speedglm)
+library(xtable)
+library(boot)
+
+source('probit-functions.R')
+# Contains the following three functions: 
+# (1) bias_dynamic_parameter, (2) bias_parameter and (3) spglm
+
+# Reading the data;
+data  <- read.dta("LFPmovers.dta")
+
+# VALUES OF THE PARAMETERS;
+N0 <- length(unique(data$id))
+T0 <- length(unique(data$year))
+P0 <- 7 # number of nonfe regressors in the specification
+P <- 7
+B <- 2 # number of bootstrap
+N <- N0 # number of individuals
+T <- T0 # number of time periods
+
+# Regression specifications
+spec_fe <- lfp ~ laglfp + kids0_2 + kids3_5 + kids6_17 + loghusbandincome + age+ age2 + factor(id) + factor(year)
+# specification for simulated data (without intercept)
+# do not include intercept because do not have it in simulation in the first place
+spec_fe_s <- lfp ~ laglfp + kids0_2 + kids3_5 + kids6_17 + loghusbandincome + age + age2 + factor(id) + factor(year) - 1
+
+# Check number of missing LFP values
+nomissing <- !is.na(data$lfp)
+pop <- length(data$lfp[nomissing])
+
+# TRIMMING PARAMETERS FOR ANALYTICAL CORRECTIONS
+L2 <- 1
+L3 <- 0
+L4 <- 0
+
+# CALIBRATION OF PARAMETERS
+deltahatfete <- spglm(spec_fe, data = data, family = binomial(link = "probit"), subset = nomissing)
+betahatfete <- deltahatfete$coef[2:(P0 + 1)] # subset the coefficients of nonfe regressors
+
+# predicted values within the link function
+index <- deltahatfete$linear.predictors 
+# X'*theta
+index0 <- index - betahatfete[1] * data$laglfp[nomissing] 
+# X'*theta + coef of laglfp
+index1 <- index + betahatfete[1] * (1 - data$laglfp[nomissing]) 
+
+# Estimated coefficients from the data
+beta0 <- betahatfete[c(1:P)]
+
+true_index0 <- matrix(index0, nrow = T, byrow = FALSE)
+true_index1 <- matrix(index1, nrow = T, byrow = FALSE)
+
+rm(deltahatfete, index, index0, index1)
+
+# VARIABLE INITIALIZIN
+
+betasfete <- matrix(0, nrow = B, ncol = P)
+betasabc <- matrix(0, nrow = B, ncol = P)
+betasjbc <- matrix(0, nrow = B, ncol = P)
+betascbc <- matrix(0, nrow = B, ncol = P)
+se_betasfete <- matrix(0, nrow = B, ncol = P)
+
+# (1) Data Simulation
+data_gen <- function(data, mle) {
+  # CREATE OUTCOME VARIABLE
+  lfp_s <- matrix(0, nrow = T0, ncol = N0)
+  lfp0 <- data$laglfp[data$year == 1] #initial value
+  for (t in 1:T0) {
+    # Interpretation: lfp0*betahatfete[1] + X'*betahatfete[2:end] > rnorm(N0),
+    # Determines the lfp in the next period
+    lfp_s[t, ] <- (lfp0 * true_index1[t, ] + (1 - lfp0) * true_index0[t, ] > rnorm(N0))
+    lfp0 <- lfp_s[t, ] # update so as to simulate the next period lfp
+  }
+  # simulate lagged dependent variable
+  laglfp_s <- matrix(rbind(data$laglfp[data$year == 1], lfp_s[-T0, ]), ncol = 1, byrow = FALSE)
+  lfp_s <- matrix(lfp_s, ncol = 1, byrow = FALSE)
+  # All other covariates are taken from the original data
+  data_s <- data.frame(year = data$year, lfp = lfp_s, laglfp = laglfp_s, kids0_2 = data$kids0_2, 
+                       kids3_5 = data$kids3_5, kids6_17 = data$kids6_17, 
+                       loghusbandincome = data$loghusbandincome, age = data$age, age2 = data$age2, id = data$id)
+  return(data_s)
+}
+
+# (2) Bootstrap statistics function
+boot_test <- function(data, spec_fe_s) {
+  return(c(data[,2]))
+}
+boot_stat <- function(data, spec_fe_s) {
+  # ELIMINATING INDIVIDUALS WITH ALL 1's OR 0's
+  D <- kronecker(c(1:N0), matrix(1, T0, 1))
+  # tapply(lfp_s, D, sum): counts LFP for each individual over the spell of the years (0-9)
+  td <- kronecker(tapply(data$lfp, D, sum),  matrix(1, T0, 1)) 
+  insample <- ((td > 0) & (td < T0))
+  N <- sum(insample)/T0 # population of this simulated data
+  # SELECT SUBSAMPLE;
+  innomissing <- nomissing[insample]
+  ### Estimation using Simulated Data ###
+  # FIXED EFFECTS on the simulated data;
+  deltahatfete <- spglm(spec_fe_s, data = data[insample, ], family = binomial(link = "probit"), x = TRUE)
+  betahatfete <- deltahatfete$coef[1:P]
+  
+  # ANALYTICAL BIAS CORRECTION;
+  betahatabc <- t(betahatfete - bias_dynamic_parameter(N, T0, deltahatfete$linear.predictors, 
+                                                       deltahatfete$x[, 1:P], data$lfp[insample], 
+                                                       L2 = L2, L3 = L3, L4 = L4))               
+  # SPLIT-PANEL JACKKNIFE IN BOTH DIMENSIONS;
+  # LEAVING HALF PANEL OUT, ALL TIME PERIODS
+  # i = 1, ..., N/2, j = 1, ..., T
+  index <- (data$id <= median(data$id[nomissing & insample]))
+  deltahat11 <- spglm(spec_fe_s, data = data[index, ], family = binomial(link = "probit"))
+  betahat11 <- deltahat11$coef[1:P]
+  # i = N/2 + 1, ..., N, t = 1, ..., T
+  index <- (data$id >= median(data$id[nomissing & insample]))  
+  deltahat12 <- spglm(spec_fe_s, data = data[index, ], family = binomial(link = "probit"))
+  betahat12 <- deltahat12$coef[1:P]
+  
+  # LEAVING HALF PANEL OUT, ALL INDIVIDUALS
+  # i = 1, ..., N, j = 1, ..., T/2
+  index <- (data$year <= median(data$year[nomissing & insample]))  
+  deltahat21 <- spglm(spec_fe_s, data = data[index, ], family = binomial(link = "probit"))
+  betahat21 <- deltahat21$coef[1:P]
+  # i = 1, ..., N, j = T/2 + 1, ..., T
+  index <- (data$year >= median(data$year[nomissing & insample]))  
+  deltahat22 <- spglm(spec_fe_s, data = data[index, ], family = binomial(link = "probit"))
+  betahat22 <- deltahat22$coef[1:P]
+  # SBC (Fernandez-Val and Weidner, 2016)
+  betahatjbc <- 3*betahatfete - (betahat11 + betahat12 + betahat21 + betahat22)/2    
+  
+  # CROSS-OVER JACKKNIFE;
+  # FIRST CROSS: i = 1, ..., N/2, j = 1, ..., T/2; i = N/2 + 1, ..., N, j = T/2 + 1, ..., T
+  index <- (data$id <= median(data$id[nomissing & insample]) & data$year <= median(data$year[nomissing & insample])) | 
+    (data$id >= median(data$id[nomissing & insample]) & data$year >= median(data$year[nomissing & insample]))
+  deltahat1 <- spglm(spec_fe_s, data = data[index, ], family = binomial(link = "probit"))
+  betahat1 <- deltahat1$coef[1:P]
+  # SECOND CROSS: i = 1, ..., N/2,  j = T/2 + 1, ..., T; i = N/2 + 1, ..., N, j = 1, ..., T/2
+  index <- (data$id <= median(data$id[nomissing & insample]) & data$year >= median(data$year[nomissing & insample])) | 
+    (data$id >= median(data$id[nomissing & insample]) & data$year <= median(data$year[nomissing & insample]))
+  deltahat2 <- spglm(spec_fe_s,  data = data[index, ], family = binomial(link = "probit"))
+  betahat2 <- deltahat2$coef[1:P]
+  # CBC
+  betahatcbc <- 2*betahatfete - (betahat1 + betahat2)/2    
+  # STANDARD ERRORS;
+  se_betahatfete <- summary(deltahatfete)$coeff[1:P, 2]
+  # return output
+  return(c(betahatfete, betahatabc, betahatjbc, betahatcbc, se_betahatfete))
+}
+set.seed(88)
+sim_fe <- boot(data = data, statistic = boot_stat, sim = "parametric", ran.gen = data_gen, mle = 0, 
+               spec_fe_s = spec_fe_s, parallel = "multicore", ncpus = 1, R = B)
--- Simulation Code/dynamic-linear-panel-June-2020.R
+++ Simulation Code/dynamic-linear-panel-June-2020.R
@@ -22,10 +22,12 @@ library(foreign)
 library(xtable)
 library(plm)
 library(gmm)
+library(boot)
 
 data2 <- read.dta("democracy-balanced-l4.dta")
 data2 <- pdata.frame(data2, index = c("id", "year"))
-
+N <- length(unique(data2$id))
+T <- length(unique(data2$year))
 ################################
 ## Calibration by Fixed Effects
 ################################
@@ -40,6 +42,7 @@ data2$l4lgdp <- lag(data2$lgdp, 4)
 ##### 1. FE using data at hand ####
 fit <- lm(form, data2) 
 coefs <- coef(fit)[2:6]
+sigma <- summary(fit)$sigma
 # Long run effects of treatment
 lr <- coefs[1]/(1 - sum(coefs[2:5]))
 
@@ -52,10 +55,28 @@ sdata$l2lgdp <- 0
 sdata$l3lgdp <- 0
 sdata$l4lgdp <- 0
 index0 <- predict(fit, sdata)
+index0 <- matrix(index0, nrow = T - 4, ncol = N)
 rm(sdata)
 
 ## Auxiliary Functions 
 # (a) Generate data sets
+data.rg <- function(data, coef = coefs, sig = sigma, index = index0, mle) {
+  N <- length(unique(data$id))
+  T <- length(unique(data$year))
+  coef <- coef[-1]
+  slgdp <- matrix(data$lgdp, nrow = T, ncol = N)
+  # Generate simulated dependent variable from t=5 
+  for (t in 5) {
+    slgdp[t, ] <- index[t - 4, ] + coefs[1]*slgdp[t - 1, ] + coefs[2]*slgdp[t - 2, ] + coefs[3]*slgdp[t - 3, ] + coefs[4]*slgdp[t - 4, ] + sig*rnorm(N)
+  }
+  data_b <- data
+  # Replace with simulated dependent variable
+  data_b$lgdp <- matrix(slgdp, ncol = 1)
+  data_b <- data.frame(data_b)
+  data_b <- pdata.frame(data_b, index = c("id", "year"))  
+  return(data_b)
+}
+
 data_rg <- function(data, fit_original = fit, index0 = index0, mle) {
   N <- length(unique(data$id))
   T <- length(unique(data$year))
@@ -75,6 +96,10 @@ data_rg <- function(data, fit_original = fit, index0 = index0, mle) {
   data_b <- data.frame(data_b)
   data_b <- pdata.frame(data_b, index = c("id", "year"))    
   return(data_b)
+}
+
+check <- function(data, form_fe, form_abc) {
+  return(names(data))
 }
 # (b) Analytical Bias correction
 abc <- function(data, form, lags, N) {
@@ -97,208 +122,75 @@ abc <- function(data, form, lags, N) {
   return(as.vector(bias/T))
 }
 
-# (c) Split Sample Jackknife
+# (c) Split Sample Jackknife (F-V and Weidner, 2016)
 sbc <- function(data, form) {
   N <- length(unique(data$id))
   T <- length(unique(data$year))
+  unit <- as.double(data$id)
+  time <- as.double(data$year)
   # Full sample estimation
   whole <- plm(form, data, model = "within", effect = "twoways", index = c("id", "year"))
   # Split across time series dimension
-  t_lower <- plm(form, data, subset = (as.double(data$year) <= ceiling(T/2)), 
-                 model = "within", effect = "twoways", index = c("id", "year"))
-  t_upper <- plm(form, data, subset = (as.double(data$year) >= floor(1 + T/2)), 
-                 model = "within", effect = "twoways", index = c("id", "year"))
+  t_lower <- plm(form, data = data[time <= ceiling(T/2), ], model = "within", 
+                 effect = "twoways", index = c("id", "year"))
+  t_upper <- plm(form, data = data[time >= floor(1 + T/2), ], model = "within", 
+                 effect = "twoways", index = c("id", "year"))
   bc_t <- 0.5*(coef(t_lower) + coef(t_upper))
   # Split across individual dimension
-  n_lower <- plm(form, data, subset = (as.double(data$id) <= ceiling(N/2)), 
-                 model = "within", effect = "twoways", index = c("id", "year"))
-  n_upper <- plm(form, data, subset = (as.double(data$id) >= floor(1 + N/2)), 
-                 model = "within", effect = "twoways", index = c("id", "year"))
+  n_lower <- plm(form, data = data[unit <= ceiling(N/2), ], model = "within", 
+                 effect = "twoways", index = c("id", "year"))
+  n_upper <- plm(form, data = data[unit >= floor(1 + N/2), ], model = "within", 
+                 effect = "twoways", index = c("id", "year"))
   bc_n <- 0.5*(coef(n_lower) + coef(n_upper))
   # SBC bias correction
   coef_sbc <- 3*coef(whole) - bc_t - bc_n
   return(coef_sbc)
 }
 
-# (d) Cross-over Bias Correction
+# (d) Cross-over Bias Correction (New Proposal)
 cbc <- function(data, form) {
-  N <- length(unique(data$id))
-  T <- length(unique(data$year))
+  unit <- as.double(data$id)
+  time <- as.double(data$year)
   # Full sample estimation
   whole <- plm(form, data, model = "within", effect = "twoways", index = c("id", "year"))
   # Two cross-over subsamples
-  subsample1 <- as.double(data$id) <= 
-  subsample2 
+  subsample1 <- (unit <= (median(unit)) & time <= median(time)) | 
+    (unit >= median(unit) & time >= median(time))
+  subsample2 <- (unit <= median(unit) & time >= median(time)) | 
+    (unit >= median(unit) & time <= median(time))
+  cross1 <- plm(form, data = data[subsample1, ], model = "within", effect = "twoways", index = c("id", "year"))
+  cross2 <- plm(form, data = data[subsample2, ], model = "within", effect = "twoways", index = c("id", "year"))
+  coef_cbc <- 2*coef(whole) - 0.5*(coef(cross1) + coef(cross2))
+  return(coef_cbc)
 }
 #### 3. Simulation ####
 R <- 500
 
-## FIXED EFFECTS APPROACH 
 ## statistics to be computed in each bootstrap draw ##
-sim_fe <- function(data, form_fe, form_abc){
+boot_fe <- function(data, form_fe, form_abc) {
   # Fixed Effects
   fe_fit <- plm(form_fe, data, model = "within", effect = "twoways", index = c("id", "year"))
   coefs_fe <- coef(fe_fit)
-  lr_fe <- coefs_fe[1]/(1 - sum(coefs_fe[2:5]))
-  # jackknife bias correction
-  fe_fit1 <- plm(form_fe, data, subset = (as.double(year) <= 14), model = "within", 
-                 effect = "twoways", index = c("id", "year"))
-  fe_fit2 <- plm(form_fe, data, subset = (as.double(year) >= 14), model = "within", 
-                 effect = "twoways", index = c("id", "year"))
-  coefs_jbc  <- 19*coef(fe_fit)/9 - 10*(coef(fe_fit1) + coef(fe_fit2))/18
-  lr_fe1     <- coef(fe_fit1)[1]/(1 - sum(coef(fe_fit1)[2:5]))
-  lr_fe2     <- coef(fe_fit2)[1]/(1 - sum(coef(fe_fit2)[2:5]))
-  lr_jbc     <- 19*lr_fe/9 - 10*(lr_fe1 + lr_fe2)/18
-  
-  # Analytical bias correction
-  bias_l4 <- abc(data, form_abc, lags = 4, N = length(levels(id)))
-  coefs_abc4 <- coefs_fe - bias_l4
-  jac_lr <- c(1,rep(lr_fe, 4))/(1 - sum(coefs_fe[2:5]))
-  lr_abc4 <- lr_fe - crossprod(jac_lr, bias_l4)
-  
-  return(c(coefs_fe, coefs_jbc, coefs_abc4, lr_fe, lr_jbc, lr_abc4))
+  # abc
+  bias_l4 <- abc(data, form = form_abc, lags = 4, N = length(levels(data$id)))
+  coefs_abc4 <- coefs_fe - bias_l4 
+  # sbc
+  coefs_sbc <- sbc(data, form = form_fe)
+  # cbc
+  coefs_cbc <- cbc(data, form = form_fe)
+  # Return outputs
+  return(c(coefs_fe, coefs_abc4, coefs_sbc, coefs_cbc))
 }
-
-library(boot) # library to do bootstrap with paralell computing
 
 set.seed(888)
-form_fe <- lgdp ~ dem + lag(lgdp, 1:4) - 1
+form_fe <- lgdp ~ dem + l1lgdp + l2lgdp + l3lgdp + l4lgdp + factor(year) + factor(id) - 1
 form_abc <- lgdp ~ dem + l1lgdp + l2lgdp + l3lgdp + l4lgdp + factor(year) + factor(id)
-result.sim.fe <- boot(data = data, statistic = sim_fe, sim = "parametric", ran.gen = data_rg, 
-                      mle = 0, form_fe = form_fe, form_abc = form_abc,  parallel = "multicore", 
-                      ncpus = 10, R = R)
+
+sim_fe <- boot(data = data2, statistic = boot_fe, sim = "parametric", ran.gen = data.rg, 
+               mle = 0, form_fe = form_fe, form_abc = form_abc,  parallel = "multicore", 
+               ncpus = 1, R = 5)
+
 # robust estimator of std deviation based on IQR
-rsd <- function(x) {return((quantile(x, .75, na.rm = TRUE) - quantile(x, .25, na.rm = TRUE))/(qnorm(.75) - qnorm(.25)))} 
-
-# Robust bootstrap std errors;
-result      <- structure(vapply(result.boot.SE.fe$t, as.double, numeric(1)), dim=dim(result.boot.SE.fe$t)); # transforms "Error in La.svd(x, nu, nv) : error code 1 from Lapack routine 'dgesdd'\n" to NA
-bse.fe      <- apply(result[,1:5], 2, rsd);
-bse.jbc     <- apply(result[,6:10], 2, rsd);
-bse.abc4    <- apply(result[,11:15], 2, rsd);
-bse.lr.fe   <- rsd(result[,16]);
-bse.lr.jbc  <- rsd(result[,17]);
-bse.lr.abc4 <- rsd(result[,18]);
-
-## ARELLANO-BOND APPROACH #######################################################################
-########## statistics to be computed in each bootstrap draw #####################################
-boot.SE.ab <- function(data, form.ab){
-  
-  # Arellano-Bond
-  N         <- length(unique(data$id));
-  ab.fit <- pgmm(form.ab, data, model = "onestep", effect = "twoways" );
-  coefs.ab  <- coef(ab.fit); 
-  lr.ab     <- coefs.ab[1]/(1-sum(coefs.ab[2:5]));
-  
-  # Split sample bias correction - 1 partition
-  S2        <- 1;
-  acoeff.ab <- 0*coef(ab.fit);
-  alr.ab    <- 0;
-  for (s in 1:S2) {
-    sample1   <- sample(N, ceiling(N/2), replace = FALSE);
-    ab.fit1 <- pgmm(form.ab, data[as.double(id) %in% sample1, ], model = "onestep", effect = "twoways" );
-    ab.fit2 <- pgmm(form.ab, data[!(as.double(id) %in% sample1), ], model = "onestep", effect = "twoways" );
-    lr.ab1     <- coef(ab.fit1)[1]/(1-sum(coef(ab.fit1)[2:5]));
-    lr.ab2     <- coef(ab.fit2)[1]/(1-sum(coef(ab.fit2)[2:5]));
-    acoeff.ab <- acoeff.ab + ((coef(ab.fit1) + coef(ab.fit2))/2)/S2;
-    alr.ab    <- alr.ab + ((lr.ab1 + lr.ab2)/2)/S2;
-  }
-  coefs.ab.jbc  <- 2*coef(ab.fit) - acoeff.ab;
-  lr.ab.jbc     <- 2*lr.ab - alr.ab;
-  
-  # Split sample bias correction - 5 partitions
-  S2        <- 5;
-  acoeff.ab <- 0*coef(ab.fit);
-  alr.ab    <- 0;
-  for (s in 1:S2) {
-    sample1   <- sample(N, ceiling(N/2), replace = FALSE);
-    ab.fit1 <- pgmm(form.ab, data[as.double(id) %in% sample1, ], model = "onestep", effect = "twoways" );
-    ab.fit2 <- pgmm(form.ab, data[!(as.double(id) %in% sample1), ], model = "onestep", effect = "twoways" );
-    lr.ab1     <- coef(ab.fit1)[1]/(1-sum(coef(ab.fit1)[2:5]));
-    lr.ab2     <- coef(ab.fit2)[1]/(1-sum(coef(ab.fit2)[2:5]));
-    acoeff.ab <- acoeff.ab + ((coef(ab.fit1) + coef(ab.fit2))/2)/S2;
-    alr.ab    <- alr.ab + ((lr.ab1 + lr.ab2)/2)/S2;
-  }
-  coefs.ab.jbc5  <- 2*coef(ab.fit) - acoeff.ab;
-  lr.ab.jbc5     <- 2*lr.ab - alr.ab;
-  
-  
-  return(c(coefs.ab[1:5], coefs.ab.jbc[1:5], coefs.ab.jbc5[1:5], lr.ab, lr.ab.jbc, lr.ab.jbc5));
+rsd <- function(x) {
+  return((quantile(x, .75, na.rm = TRUE) - quantile(x, 0.25, na.rm = TRUE))/(qnorm(0.75) - qnorm(0.25)))
 }
-
-library(boot); # library to do bootstrap with paralell computing
-
-set.seed(888);
-result.boot.SE.ab <- boot(data = data, statistic=boot.SE.ab, sim = "parametric", ran.gen = data.rg, mle = 0, form.ab = form.ab, 
-                          parallel="multicore", ncpus = 20, R=R);
-
-# Robust bootstrap std errors;
-
-result      <- structure(vapply(result.boot.SE.ab$t, as.double, numeric(1)), dim=dim(result.boot.SE.ab$t)); # transforms "Error in La.svd(x, nu, nv) : error code 1 from Lapack routine 'dgesdd'\n" to NA
-bse.ab      <- apply(result[,1:5], 2, rsd);
-bse.ab.jbc  <- apply(result[,6:10], 2, rsd);
-bse.ab.jbc5 <- apply(result[,11:15], 2, rsd);
-bse.lr.ab   <- rsd(result[,16]);
-bse.lr.ab.jbc  <- rsd(result[,17]);
-bse.lr.ab.jbc5  <- rsd(result[,18]);
-
-
-
-######## Table of results;
-
-options(digits=2);
-table.all <- matrix(NA, nrow = 18, ncol = 6, dimnames = list(c("Democracy", "CSE", "BSE", "L1.log(gdp)",  "CSE1", "BSE1", "L2.log(gdp)",  "CSE2", "BSE2","L3.log(gdp)",  "CSE3", "BSE3", "L4.log(gdp)",  "CSE4", "BSE4", "LR-Democracy","CSE5","BSE5"), c("FE", "SBC", "ABC4", "AB", "SBC1", "SBC5" )));
-
-
-table.all[c(1,4,7,10,13), 1] <- coefs.fe[1:5];
-table.all[c(2,5,8,11,14), 1] <- cse.fe[1:5];
-table.all[c(3,6,9,12,15), 1] <- bse.fe[1:5];
-
-table.all[c(1,4,7,10,13), 2] <- coefs.jbc[1:5];
-#table.all[c(2,5,8,11,14), 2] <- cse.fe[1:5];
-table.all[c(3,6,9,12,15), 2] <- bse.jbc[1:5];
-
-table.all[c(1,4,7,10,13), 3] <- coefs.abc4[1:5]
-table.all[c(3,6,9,12,15), 3] <- bse.abc4[1:5]
-
-table.all[c(1,4,7,10,13), 4] <- coefs.ab[1:5]
-table.all[c(2,5,8,11,14), 4] <- cse.ab[1:5]
-table.all[c(3,6,9,12,15), 4] <- bse.ab[1:5]
-
-table.all[c(1,4,7,10,13), 5] <- coefs.ab.jbc[1:5]
-table.all[c(3,6,9,12,15), 5] <- bse.ab.jbc[1:5]
-
-table.all[c(1,4,7,10,13), 6] <- coefs.ab.jbc5[1:5]
-table.all[c(3,6,9,12,15), 6] <- bse.ab.jbc5[1:5]
-
-
-table.all[16, 1] <- lr.fe
-table.all[17, 1] <- cse.lr.fe
-table.all[18, 1] <- bse.lr.fe
-
-table.all[16, 2] <- lr.jbc
-#table.all[17, 2] <- cse.lr.fe
-table.all[18, 2] <- bse.lr.jbc
-
-table.all[16, 3] <- lr.abc4
-table.all[18, 3] <- bse.lr.abc4
-
-table.all[16, 4] <- lr.ab
-table.all[18, 4] <- bse.lr.ab
-
-table.all[16, 5] <- lr.ab.jbc
-table.all[17, 5] <- cse.lr.ab
-table.all[18, 5] <- bse.lr.ab.jbc
-
-table.all[16, 6] <- lr.ab.jbc5
-table.all[18, 6] <- bse.lr.ab.jbc5
-
-
-table.all[1, ] <- 100 * table.all[1, ]
-table.all[2, ] <- 100 * table.all[2, ]
-table.all[3, ] <- 100 * table.all[3, ]
-table.all[16, ] <- 100 * table.all[16, ]
-table.all[17, ] <- 100 * table.all[17, ]
-table.all[18, ] <- 100 * table.all[18, ]
-
-
-xtable(table.all, digits=2)
