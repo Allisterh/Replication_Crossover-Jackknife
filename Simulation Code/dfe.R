3bdb74f960f96122f743583bf3338c649f518793
Shuowen Chen <swchen@bu.edu>
2020-07-12 15:29:47 -0400

    Update application code

--- Simulation Code/dfe.R
+++ Simulation Code/dfe.R
@@ -1,4 +1,4 @@
-# 7/3/2020 Shuowen Chen
+# 7/12/2020 Shuowen Chen
 # The script contains code that implement debiased fixed effects and panel bootstrap 
 # standard errors for dataset in Chernozhukov, Kasahara and Schrimpf (2020)
 # Serves as application for Chen, Chernozhukov, Fernandez-Val, Kasahara and Schripmf (2020)
@@ -70,7 +70,7 @@ multiple_split <- function(s, n, unit, time, dat, uc, fm) {
 # Cross-over jackknife with overlapping in the tiem dimension
 # q: fraction of time periods used in each subpabel,
 #    default is 0.5. Larger than 0.5 features overlap in the 
-#    time periods used. 
+#    time periods used: fraction is 2*q - 1
 crossover <- function(unit, time, dat, uc, fm, q = 0.5) {
   t_cutoff <- c(floor(quantile(time, q)), 
                 ceiling(quantile(time, 1 - q)))
@@ -85,6 +85,27 @@ crossover <- function(unit, time, dat, uc, fm, q = 0.5) {
   # debiased estimate
   bc <- uc/(1 - q) - 0.5*(coef(cross1) + coef(cross2))*q/(1 - q)
   return(bc)
+}
+
+hybrid_mbcovp <- function(s, n, unit, time, dat, uc, fm, q) {
+  across <- 0 * uc
+  t_cutoff <- c(floor(quantile(time, q)), 
+                ceiling(quantile(time, 1 - q)))
+  for (k in 1:s) {
+    sample1 <- sample(n, ceiling(n/2), replace = FALSE)
+    subsample1 <- (unit %in% sample1 & time <= t_cutoff[1]) | 
+      (!(unit %in% sample1) & time >= t_cutoff[2])
+    subsample2 <- (unit %in% sample1 & time >= t_cutoff[2]) | 
+      (!(unit %in% sample1) & time <= t_cutoff[1])
+    cross1 <- felm(fm, data = dat[subsample1, ], 
+                   weights = dat[subsample1, ]$sweight)
+    cross2 <- felm(fm, data = dat[subsample2, ],
+                   weights = dat[subsample2, ]$sweight)
+    across <- across + ((coef(cross1) + coef(cross2))/2)/s
+  }
+  # average cross over corrected
+  acbc <- uc/(1 - q) - across*q/(1 - q)
+  return(acbc)
 }
 
 ##### DFE estimation based on cross-over jackknife
@@ -147,6 +168,7 @@ reg_fe <- function(df, # data
               ovp = coef_overlap, sumpolicy = peff, sumbehavior = beff))
 }
 
+
 # The following function extends the function mainregressions
 # NOTE: there is one change in the code
 # the original code put xlist in the controls, but xlist is specified as an empty list
@@ -163,7 +185,7 @@ mainregressions_fe <- function(df, # data
                                tvars, # key confounder from SIR
                                ivlist = "0",
                                L = 14, 
-                               frac = 2/3,
+                               frac = 0.6,
                                num_split = 5,
                                fixed) {
   # This is considering both pols and interacted of pmask with months
@@ -256,21 +278,6 @@ infovars <- list(c("dlogdc", "logdc"),
 infovarsd <- list(c("dlogdd", "logdd"),
                   c("dlogdd", "logdd", "dlogdd.national", "logdd.national")) # deaths
 
-# Regression
-#case <- mainregressions_fe(sdf, yvar, pols, bvars, infovars, tvars, L = 14, fixed = "state + month")
-#death_case <- mainregressions_fe(sdf, yvar2, pols, bvars, infovars, NULL, L = 21, fixed = "state + month")
-#death_death <- mainregressions_fe(sdf, yvar2, pols, bvars, infovarsd, NULL, L = 21, fixed = "state + month")
-
-# Week and state fixed effects
-#case_week <- mainregressions_fe(sdf, yvar, pols, bvars, infovars, tvars, L = 14, fixed = "state + week")
-#death_case_week <- mainregressions_fe(sdf, yvar2, pols, bvars, infovars, NULL, L = 21, fixed = "state + week")
-#death_death_week <- mainregressions_fe(sdf, yvar2, pols, bvars, infovarsd, NULL, L = 21, fixed = "state + week")
-
-# Consider date and state fixed effect. Drop information structure as there is no policy variation
-#case_date <- mainregressions_fe(sdf, yvar, pols, bvars, NULL, tvars, L = 14, fixed = "state + date")
-#death_date <- mainregressions_fe(sdf, yvar2, pols, bvars, NULL, NULL, L = 21, fixed = "state + date")
-
-
 #### 2. Bootstrap Standard Errors 
 # Compute estimates in each bootstrap
 bootstat_case <- function(data) {
@@ -291,7 +298,7 @@ bootstat_deathdeath <- function(data) {
 
 # Call boot command to conduct bootstrap
 set.seed(88) # seed for replication
-num_boot <- 5 # number of bootstraps
+num_boot <- 500 # number of bootstraps
 ncores <- 1 # number of cpus (speed)
 
 # nonpar will throw warning wrt multiple splitting bias correction
@@ -302,14 +309,6 @@ case_nonpar <- boot(data = sdf, statistic = bootstat_case, sim = "parametric",
 case_wb <- boot(data = sdf, statistic = bootstat_case, sim = "parametric", 
                 ran.gen = data_wb, mle = 0, parallel = "multicore", 
                 ncpus = ncores, R = num_boot)
-
-deathcase_nonpar <- boot(data = sdf, statistic = bootstat_deathcase, 
-                         sim = "parametric", ran.gen = data_rg, mle = 0, 
-                         parallel = "multicore", ncpus = ncores, R = num_boot)
-
-deathcase_wb <- boot(data = sdf, statistic = bootstat_deathcase, 
-                     sim = "parametric", ran.gen = data_wb, mle = 0, 
-                     parallel = "multicore", ncpus = ncores, R = num_boot)
 
 deathdeath_nonpar <- boot(data = sdf, statistic = bootstat_deathdeath, 
                           sim = "parametric", ran.gen = data_rg, mle = 0, 
@@ -340,143 +339,4 @@ bsemat <- function(bootlist) {
   ref <- rbind(ref, num_star)
   return(ref)
 }
-
-#### Produce tables 
-
-# auxiliary function for table productions
-# 1. Filling tables
-fill <- function(mat, covnames, type) {
-  numcov <- length(covnames)
-  if (type == "pib") {
-    # First four columns: bvar1 
-    tab <- matrix(NA, ncol = 16, nrow = 3*numcov)
-    # fill in contents
-    for (i in (1:16)) {
-      tab[1:(3*numcov - 3), i] <- matrix(mat[, (1 + (i - 1)*(numcov - 1)):(i*(numcov - 1))], 
-                                         ncol = 1)
-    }
-    for (i in (1:4)) {
-      tab[(3*numcov - 2):(3*numcov), (4*i - 3):(4*i)] <- matrix(mat[, (4*i*numcov - 3):(4*i*numcov)],
-                                                                nrow = 3, ncol = 4)
-    }
-  } else if (type == "pbiy") {
-    tab <- matrix(NA, ncol = 4, nrow = 3*numcov)
-    for (i in (1:4)) {
-      tab[1:(3*numcov - 6), i] <- matrix(mat[, (1 + (i - 1)*(numcov - 2)):(i*(numcov - 2))],
-                                         ncol = 1)
-      # fill in sumpol and wsumb
-      tab[(3*numcov - 5):(3*numcov - 3), i] <- matrix(mat[, 4*(numcov - 2) + i])
-      tab[(3*numcov - 2):(3*numcov), i] <- matrix(mat[, 4*(numcov - 1) + i])
-    }
-  } else {
-    tab <- matrix(NA, ncol = 4, nrow = 3*numcov)
-    for (i in (1:4)) {
-      tab[1:(3*numcov - 3), i] <- matrix(mat[, (1 + (i - 1)*(numcov - 1)):(i*(numcov - 1))],
-                                         ncol = 1)
-      # fill in sumpol
-      tab[(3*numcov - 2):(3*numcov), i] <- matrix(mat[, 4*(numcov - 1) + i])
-    }
-  }
-  # assign colnames and rownames
-  colnames(tab) <- rep(c("nobc","bc","mbc","ovp"), ncol(tab)/4)
-  rownames(tab) <- rep("bse", nrow(tab))
-  for (i in (1:length(covnames))) {
-    rownames(tab)[1 + 3*(i - 1)] <- covnames[i]
-    rownames(tab)[3*i] <- "stars"
-  }
-  tab <- round(tab, digits = 3)
-  # adding parentheses to bse
-  for (i in 1:nrow(tab)) {
-    if (rownames(tab)[i] == "bse") tab[i, ] <- paste0("(", format(unlist(tab[i, ])), ")")
-  }
-  return(tab)
-}
-
-pols_interact <- c("pmask.april","pmask.may", pols[-1])
-
-# Depending on what regression type, produce a correspondent table
-# lag: number of lags for covariates
-tableproduction <- function(ref, type = c("pib", "pbiy", "piy"), pols, 
-                            pols_interact, bvars, tvars, infovars, 
-                            lag) {
-  type <- match.arg(type)
-  # rows for pib
-  xpib1 <- c(pols, infovars[[1]], "sumpol")
-  xpib3 <- c(pols, infovars[[2]], "sumpol")
-  s1 <- length(xpib1)*16
-  s2 <- length(xpib3)*16
-  # Lagged covariates name for pbiy and piy
-  lag_pol <- sprintf("lag(%s, %d)", pols, lag)
-  lag_b <- sprintf("lag(%s, %d)", bvars, lag)
-  lag_pol_int <- sprintf("lag(%s, %d)", pols_interact, lag)
-  lag_info1 <- sprintf("lag(%s, %d)", infovars[[1]], lag)
-  lag_info3 <- sprintf("lag(%s, %d)", infovars[[2]], lag)
-  # rows for pbiy
-  xpbiy1 <- c(lag_pol, lag_b, lag_info1, tvars, "sumpol", "wsumb")
-  xpbiy3 <- c(lag_pol, lag_b, lag_info3, tvars, "sumpol", "wsumb")
-  xpbiy1_int <- c(lag_pol_int, lag_b, lag_info1, tvars, "sumpol", "wsumb")
-  xpbiy3_int <- c(lag_pol_int, lag_b, lag_info3, tvars, "sumpol", "wsumb")
-  s3 <- length(xpbiy1)*4 # number for pbiy case info1
-  s4 <- length(xpbiy1_int)*4
-  s5 <- length(xpbiy3)*4
-  s6 <- length(xpbiy3_int)*4
-  # rows for piy
-  xpiy1 <- c(lag_pol, lag_info1, tvars, "sumpol")
-  xpiy3 <- c(lag_pol, lag_info3, tvars, "sumpol")
-  xpiy1_int <- c(lag_pol_int, lag_info1, tvars, "sumpol")
-  xpiy3_int <- c(lag_pol_int, lag_info3, tvars, "sumpol")
-  piy_length <- c(length(xpiy1)*4, length(xpiy1_int)*4,
-                  length(xpiy3)*4, length(xpiy3_int)*4)
-  if (type == "pib") {
-    # subset from ref
-    pib_info1 <- ref[, 1:s1]
-    pib_info3 <- ref[, (s1 + 161):(s1 + 160 + s2)]
-    # return two tables: one for info1, another for info3
-    # cols: four behaviors as dependent, each has nobc, bc, mbc and ovp
-    # rows: 3 * num of pols and info (estimate, bse and stars)
-    tab_info1 <- fill(pib_info1, xpib1, type = type)
-    tab_info3 <- fill(pib_info3, xpib3, type = type)
-    output <- list(pib_info1 = tab_info1, pib_info3 = tab_info3)
-  } else if (type == "pbiy") {
-    # subset from ref
-    pbiy_info1 <- ref[, (s1 + s2 + 353):(s1 + s2 + 352 + s3)]
-    pbiy_info1_int <- ref[, (s1 + s2 + 353 + s3):(s1 + s2 + 352 + s3 + s4)]
-    pbiy_info3 <- ref[, (s1 + s2 + 353 + s3 + s4):(s1 + s2 + 352 + s3 + s4 + s5)]
-    pbiy_info3_int <- ref[, (s1 + s2 + 353 + s3 + s4 + s5):(s1 + s2 + 352 + s3 + s4 + s5 + s6)]
-    tab_info1 <- fill(pbiy_info1, xpbiy1, type = type)
-    tab_info1_int <- fill(pbiy_info1_int, xpbiy1_int, type = type)
-    tab_info3 <- fill(pbiy_info3, xpbiy3, type = type)
-    tab_info3_int <- fill(pbiy_info3_int, xpbiy3_int, type = type)
-    output <- list(pbiy_info1 = tab_info1, pbiy_info1_int = tab_info1_int,
-                   pbiy_info3 = tab_info3, pbiy_info3_int = tab_info3_int)
-  } else {
-    # subset from ref
-    initial <- s1 + s2 + 353 + s3 + s4 + s5 + s6
-    piy_info1 <- ref[, initial:(initial + piy_length[1] - 1)]
-    piy_info1_int <- ref[, (initial + piy_length[1]):(initial + sum(piy_length[1:2]) - 1)]
-    piy_info3 <- ref[, (initial + sum(piy_length[1:2])):((initial + sum(piy_length[1:3]) - 1))]
-    piy_info3_int <- ref[, (initial + sum(piy_length[1:3])):((initial + sum(piy_length) - 1))]
-    tab_info1 <- fill(piy_info1, xpiy1, type = type)
-    tab_info1_int <- fill(piy_info1_int, xpiy1_int, type = type)
-    tab_info3 <- fill(piy_info3, xpiy3, type = type)
-    tab_info3_int <- fill(piy_info3_int, xpiy3_int, type = type)
-    output <- list(piy_info1 = tab_info1, piy_info1_int = tab_info1_int, 
-                   piy_info3 = tab_info3, piy_info3_int = tab_info3_int)
-  }
-  return(output)
-}
-
-# Call bsemat to get inputs from the boot results
-mat_case_nonpar <- bsemat(case_nonpar)
-mat_case_wb <- bsemat(case_wb)
-mat_deathcase_nonpar <- bsemat(deathcase_nonpar)
-mat_deathcase_wb <- bsemat(deathcase_wb)
-mat_deathdeath_nonpar <- bsemat(deathdeath_nonpar)
-mat_deathdeath_wb <- bsemat(deathdeath_wb)
-
-# Call producetable function to get table
-case_nonpar_pib <- tableproduction(mat_case_nonpar, type = "pib", pols, pols_interact,
-                                   bvars, tvars, infovars, 14)
-death_nonpar_pib <- tableproduction(mat_deathcase_wb, type = "pib", pols, pols_interact, 
-                               bvars, NULL, infovars, 21)
 
